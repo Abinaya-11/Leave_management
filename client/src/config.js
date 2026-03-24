db24533c81c03fe9178a52ad22957d49bc981d58
Abinaya-11 <saipowraj@gmail.com>
2026-03-24 22:22:41 +0530

    new features

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
-const API_URL = "http://localhost:5000";
+const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? "http://localhost:5000"
+  : "https://leave-management-backend-9fwn.onrender.com";
+
 export default API_URL;
